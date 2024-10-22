ab64b25c1c1c9e5eba59ec28f8161f9284a65df9
sungjin58 <sungjin@qntech.co.kr>
2024-10-22 11:32:24 +0900

    1

--- server/server.js
+++ server/server.js
@@ -7,9 +7,6 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
-// 정적 파일 제공
-app.use(express.static(path.join(__dirname, '../client/build')));
-
 // API 라우트 설정
 const server = http.createServer(app);
 const io = socketIo(server, {
@@ -91,11 +88,6 @@ io.on('connection', (socket) => {
       }
     });
   });
-});
-
-// 모든 요청을 React 앱으로 전달
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 server.listen(PORT, () => console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`));
